57528d8e92857736975e4217426cd189854888fa
Kai Yuan <thng.kaiyuan@gmail.com>
2016-08-13 13:39:25 +0000

    Finalize misconfiguration challenge

---
+++ misconfiguration/src/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <meta http-equiv="Content-Language" content="en-us">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CTF101 (Module website)</title>
+    </head>
+    <frameset rows="60,*" border="0">
+        <frame name="headings" src="./static/headings.html">
+        <frame name="content" src="./static/schedule.html">
+    </frameset>
+</html>
