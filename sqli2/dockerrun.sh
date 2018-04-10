687662ef3dcf75bae307bc127473a66712729d41
Kai Yuan <thng.kaiyuan@gmail.com>
2018-04-10 15:31:08 +0800

    Add workaround for issue on macos

--- sqli2/dockerrun.sh
+++ sqli2/dockerrun.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d -t -p 3009:80 ctf101:${PWD##*/}
+# workaround for https://github.com/docker/for-linux/issues/72
+unameOut="$(uname -s)"
+if [ $unameOut == "Darwin" ]; then
+    vol='-v /var/lib/mysql'
+else
+    vol=''
+fi
+docker run $vol -d -t -p 3009:80 ctf101:${PWD##*/}
